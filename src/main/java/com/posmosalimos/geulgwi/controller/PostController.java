00153fd295ad0d5cc62581359daba6dd67ebf797
sejyom <akxxkd01@gmail.com>
2023-05-18 23:44:09 +0900

    fix: postController - update

--- src/main/java/com/posmosalimos/geulgwi/controller/PostController.java
+++ src/main/java/com/posmosalimos/geulgwi/controller/PostController.java
@@ -52,7 +52,7 @@ public class PostController {
 
     //글 수정
     @PutMapping("/post/update/{seq}")
-    public String update(@PathVariable("seq")Long seq, BindingResult result, @RequestBody WriteForm form, HttpSession session) {
+    public String update(@PathVariable("seq")Long seq, BindingResult result, @RequestBody WriteForm form) {
         if (result.hasErrors()) {
             log.info("에러 발생");
             return "/post/updateForm";
