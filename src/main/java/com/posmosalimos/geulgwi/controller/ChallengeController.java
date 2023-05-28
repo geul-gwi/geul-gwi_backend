05d9529cd2bd16a51e19148789ea370aca8a299c
sejyom <akxxkd01@gmail.com>
2023-05-28 17:24:55 +0900

    fix: challenge - post user

--- src/main/java/com/posmosalimos/geulgwi/controller/ChallengeController.java
+++ src/main/java/com/posmosalimos/geulgwi/controller/ChallengeController.java
@@ -1,18 +1,43 @@
 package com.posmosalimos.geulgwi.controller;
 
+import com.posmosalimos.geulgwi.entity.Users;
 import com.posmosalimos.geulgwi.form.Challenge.ChallengePostForm;
+import com.posmosalimos.geulgwi.service.ChallengeService;
+import com.posmosalimos.geulgwi.session.SessionConst;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class ChallengeController {
 
+    private final ChallengeService challengeService;
+
     //챌린지 글 쓰기 페이지 매핑
     @GetMapping("/challenge/write")
-    public String challengeMain(Model model) {
+    public String writePostForm(Model model) {
         model.addAttribute("challengePostForm", new ChallengePostForm());
         return "/challenge/writeForm";
+    }
+
+    @PostMapping("/challenge/write")
+    public String write(@Valid @RequestBody ChallengePostForm form, BindingResult result, HttpSession session) {
+        if (result.hasErrors()) {
+            log.info("에러 발생");
+            return "/challenge/writeForm";
+        }
+        Users loginUser = (Users) session.getAttribute(SessionConst.LOGIN_USER);
+        challengeService.post(form, loginUser); //맞는 키워드값에 끼워넣어서 서비스단 작업
+        return "redirect:/";
     }
 
 }
---
+++ src/main/java/com/posmosalimos/geulgwi/service/ChallengeService.java
@@ -0,0 +1,9 @@
+package com.posmosalimos.geulgwi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChallengeService {
+
+
+}
