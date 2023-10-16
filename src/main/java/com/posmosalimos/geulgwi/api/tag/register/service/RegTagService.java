96d68394da8067329d708b9472abb4a296582d46
sejyom <akxxkd01@gmail.com>
2023-10-16 15:12:46 +0900

    fix: tag - register(tagType)

--- src/main/java/com/posmosalimos/geulgwi/api/tag/register/service/RegTagService.java
+++ src/main/java/com/posmosalimos/geulgwi/api/tag/register/service/RegTagService.java
@@ -4,6 +4,7 @@ import com.posmosalimos.geulgwi.api.tag.register.dto.RegTagDTO;
 import com.posmosalimos.geulgwi.domain.tag.constant.TagType;
 import com.posmosalimos.geulgwi.domain.tag.entity.Tag;
 import com.posmosalimos.geulgwi.domain.tag.repository.TagRepository;
+import com.posmosalimos.geulgwi.domain.user.constant.Role;
 import com.posmosalimos.geulgwi.domain.user.entity.User;
 import com.posmosalimos.geulgwi.domain.user.service.UserService;
 import com.posmosalimos.geulgwi.global.error.ErrorCode;
@@ -34,19 +35,19 @@ public class RegTagService {
         Tag tag = new Tag();
 
         if (tagRepository.findByValue(regTagDTO.getValue()) == null) { //중복이 아님(새로운 태그)
-            if (findUser.getRole().equals("ADMIN")) {
+            if (findUser.getRole() == Role.ADMIN) {
                 tag = Tag.builder()
                         .backColor(regTagDTO.getBackColor())
                         .fontColor(regTagDTO.getFontColor())
                         .value(regTagDTO.getValue())
-                        .tagType("DEFAULT")
+                        .tagType(TagType.DEFAULT.name())
                         .build();
             } else {
                 tag = Tag.builder()
                         .backColor(regTagDTO.getBackColor())
                         .fontColor(regTagDTO.getFontColor())
                         .value(regTagDTO.getValue())
-                        .tagType("USER_ADDED")
+                        .tagType(TagType.USER_ADDED.name())
                         .build();
             }
             return tagRepository.save(tag);
